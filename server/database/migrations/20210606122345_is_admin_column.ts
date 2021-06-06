bfe47bbf7f849c9c7d220c40397fbfa66d0286dc
Margus <marguslanto@gmail.com>
2021-06-06 12:36:23 +0300

    add is_admin column to users table

---
+++ server/database/migrations/20210606122345_is_admin_column.ts
@@ -0,0 +1,18 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema
+		.table('users', function (table) {
+			table.boolean('is_admin').defaultTo(false);
+		});
+}
+
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema
+		.table('users', function (table) {
+			table.dropColumn('is_admin');
+		});
+}
+
+export const config = { transaction: false };
+	
