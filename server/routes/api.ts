0a8d0280f5b9e38dcb8d27af9de3dc201bff3396
Margus <marguslanto@gmail.com>
2021-06-06 13:30:14 +0300

    add initial GET, POST and DELETE /colors endpoints

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -6,8 +6,16 @@ router.get('/', (req, res) => {
 	res.send('API home page');
 });
 
-router.get('/about', (req, res) => {
-	res.send('About API');
+router.get('/colors', (req, res) => {
+	res.send('GET /colors');
+});
+
+router.post('/colors', (req, res) => {
+	res.send('POST /colors');
+});
+
+router.delete('/colors/:colorId', (req, res) => {
+	res.send(`DELETE /colors/${req.params.colorId}`);
 });
 
 export default router;
